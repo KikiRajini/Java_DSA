bb1b5db544f17cd7fc568b8f5e218b1e9a9f0ef5
kiki <kirthanarajinikanth@gmail.com>
2026-01-27 09:29:19 +0530

    Two pointer commit day 10

--- DSA/src/Two_Pointers/TrappingRainWater.java
+++ DSA/src/Two_Pointers/TrappingRainWater.java
@@ -1,27 +1,29 @@
 package Two_Pointers;
 
 public class TrappingRainWater {
-        public int trap(int[] height) {
-            int l =0; int r = height.length-1;
-            int leftMax = height[l];
-            int rightMax = height[r];
-
-            int vol =0;
-            while(l<r){
-                if(leftMax<rightMax){
-                    l++;
-                    if(height[l]<leftMax){
-                        vol = vol + (leftMax-height[l]);
-                    }
-                    leftMax = Math.max(leftMax,height[l]);
-                }else{
-                    r--;
-                    if(height[r]<rightMax){
-                        vol = vol + (rightMax-height[r]);
-                    }
-                    rightMax = Math.max(rightMax,height[r]);
-                }
-            }
-            return vol;
+    public int trap(int[] height) {
+        if (height == null || height.length == 0) {
+            return 0;
         }
+
+        int l = 0, r = height.length - 1;
+        int leftMax = height[l], rightMax = height[r];
+        int res = 0;
+        while (l < r) {
+            if (leftMax < rightMax) {
+                l++;
+                leftMax = Math.max(leftMax, height[l]);
+                //calculating new max before the volume because if leftMax is smaller than current value, vol will be negative and we need to add extra if
+                //but now the vol will be 0 in that case. if leftmax is greater than current val..the leftmax will stay the same
+                res += leftMax - height[l];
+                //why we consider only the leftmax and not right max? also right max here may not be the true rightmax
+                //becuase we care only about the minimum.
+            } else {
+                r--;
+                rightMax = Math.max(rightMax, height[r]);
+                res += rightMax - height[r];
+            }
+        }
+        return res;
+    }
 }
