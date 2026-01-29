13c964d850d4184847617560b25b04b0fadf2c1e
kiki <kirthanarajinikanth@gmail.com>
2026-01-29 18:32:49 +0530

    Stack commit day 12

---
+++ DSA/src/Stack/MinStack.java
@@ -0,0 +1,41 @@
+package Stack;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class MinStack {
+
+        Deque<Integer> stack;
+        Deque<Integer> minStack;
+
+        public MinStack() {
+            stack = new ArrayDeque<>();
+            minStack = new ArrayDeque<>();
+        }
+
+        public void push(int val) {
+            stack.push(val);
+
+            if(minStack.isEmpty()) {
+                minStack.push(val);
+            } else {
+                minStack.push(Math.min(val, minStack.peek()));
+            }
+        }
+
+        public void pop() {
+            stack.pop();
+            minStack.pop();
+        }
+
+        public int top() {
+            return stack.peek();
+        }
+
+        public int getMin() {
+            return minStack.peek();
+        }
+
+
+}
+
