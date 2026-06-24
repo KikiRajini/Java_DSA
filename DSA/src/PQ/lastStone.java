697f5a48f1a0ac3b00334b5f4446811ec59e0a25
kiki <kirthanarajinikanth@gmail.com>
2026-06-24 09:07:23 +0530

    PQ commit day 54

---
+++ DSA/src/PQ/lastStone.java
@@ -0,0 +1,23 @@
+package PQ;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class lastStone {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int stone: stones){
+            pq.offer(stone);
+        }
+        while(pq.size()>1){
+            int a = pq.poll();
+            int b = pq.poll();
+            if(a<b){pq.offer(b-a);}
+            else if(b<a){pq.offer(a-b);}
+            else {pq.offer(0);}
+
+        }
+
+        return pq.poll();
+    }
+}
