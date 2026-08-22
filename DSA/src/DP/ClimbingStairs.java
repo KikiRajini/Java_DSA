7a9df3ee66faf0bdedfcf5a24bcd1f4a7a94610a
kiki <kirthanarajinikanth@gmail.com>
2026-08-22 08:52:03 +0530

    DP commit day 67

---
+++ DSA/src/DP/ClimbingStairs.java
@@ -0,0 +1,20 @@
+package DP;
+
+public class ClimbingStairs {
+    public int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+
+        int oneStepBehind = 2;
+        int twoStepBehind = 1;
+
+        for(int i=3; i<=n; i++){
+            int currentStep = oneStepBehind + twoStepBehind;
+            twoStepBehind = oneStepBehind;
+            oneStepBehind = currentStep;
+        }
+
+        return oneStepBehind;
+    }
+}
